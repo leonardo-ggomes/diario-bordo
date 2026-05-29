f6a37e90487002eac061444be3b19b851ce794a1
Leonardo Gomes <131769868+leonardo-ggomes@users.noreply.github.com>
2026-05-29 16:11:20 -0300

    Added .env

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Substitua com as credenciais do seu banco MySQL local
-const sequelize = new Sequelize('feedback_db', 'root', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
+    ssl: true,
+    port: process.env.DB_PORT ,
     logging: false // Desativa logs extensos no terminal
 });
 
